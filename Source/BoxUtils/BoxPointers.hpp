b62a65a3441d2d7e952ac058e67f20a73b844a92
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2018-01-29 12:22:45 +0000

    Added BoxLoops helper files. Incl:
    * ComputePack which allows to pack up several compute classes
    * BoxPointers which encapsulate information about a box

---
+++ Source/BoxUtils/BoxPointers.hpp
@@ -0,0 +1,79 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef BOXPOINTERS_HPP_
+#define BOXPOINTERS_HPP_
+
+#include "CellIndex.hpp"
+#include "FArrayBox.H"
+#include "UserVariables.hpp"
+#include <array>
+
+/// This class provides information about where a Chombo box lies in memory and
+/// how it is laid out.
+class BoxPointers
+{
+  private:
+    const int *m_in_lo;
+    const int *m_in_hi;
+
+    const int *m_out_lo;
+    const int *m_out_hi;
+
+  public:
+    std::array<const double *, NUM_VARS> m_in_ptr;
+    std::array<int, CH_SPACEDIM> m_in_stride; //!< Distance in memory between
+                                              //! two values corresponding to
+                                              //! adjacent coordinates in all
+                                              //! directions.
+
+    std::array<double *, NUM_VARS> m_out_ptr;
+    std::array<int, CH_SPACEDIM> m_out_stride; //!< Distance in memory between
+                                               //! two values corresponding to
+                                               //! adjacent coordinates in all
+                                               //! directions.
+
+    BoxPointers(const FArrayBox &in, FArrayBox &out)
+        : m_in_lo(in.loVect()), m_in_hi(in.hiVect()), m_out_lo(out.loVect()),
+          m_out_hi(out.hiVect())
+    {
+        // dataPtr in Chombo does CH_assert bound check
+        // which we don't want to do in a loop
+        for (int i = 0; i < NUM_VARS; ++i)
+            m_in_ptr[i] = in.dataPtr(i);
+        // If the output FArrayBox doesn't have all components, just don't set
+        // the pointers
+        for (int i = 0; i < out.nComp(); ++i)
+            m_out_ptr[i] = out.dataPtr(i);
+
+        m_in_stride[0] = 1;
+        m_in_stride[1] = m_in_hi[0] - m_in_lo[0] + 1;
+#if CH_SPACEDIM >= 3
+        m_in_stride[2] = (m_in_hi[1] - m_in_lo[1] + 1) * m_in_stride[1];
+#endif
+
+        m_out_stride[0] = 1;
+        m_out_stride[1] = m_out_hi[0] - m_out_lo[0] + 1;
+#if CH_SPACEDIM >= 3
+        m_out_stride[2] = (m_out_hi[1] - m_out_lo[1] + 1) * m_out_stride[1];
+#endif
+    }
+
+    CellIndexIn get_in_index(IntVect integer_coords) const
+    {
+        return (m_in_stride[2] * (integer_coords[2] - m_in_lo[2]) +
+                m_in_stride[1] * (integer_coords[1] - m_in_lo[1]) +
+                (integer_coords[0] - m_in_lo[0]));
+    }
+
+    CellIndexOut get_out_index(IntVect integer_coords) const
+    {
+        return (m_out_stride[2] * (integer_coords[2] - m_out_lo[2]) +
+                m_out_stride[1] * (integer_coords[1] - m_out_lo[1]) +
+                (integer_coords[0] - m_out_lo[0]));
+    }
+};
+
+#endif /* BOXPOINTERS_HPP_ */
---
+++ Source/BoxUtils/ComputePack.hpp
@@ -0,0 +1,75 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef COMPUTEPACK_HPP_
+#define COMPUTEPACK_HPP_
+
+#include "Cell.hpp"
+#include "DebuggingTools.hpp"
+
+/// This class can be use to bundle up several compute classes so that their
+/// tasks can all be done in one loop through box
+template <typename... compute_ts> class ComputePack;
+
+/// This function bundles up all its arguments into a compute pack.
+/// In general this will be easier to call than the constructors of ComputePack
+template <typename... compute_ts>
+ComputePack<compute_ts...> make_compute_pack(compute_ts... compute_classes)
+{
+    return ComputePack<compute_ts...>(
+        std::make_tuple(std::forward<compute_ts>(compute_classes)...));
+}
+
+template <typename... compute_ts> class ComputePack
+{
+    std::tuple<compute_ts...> m_compute_tuple;
+
+    // Begin: Helper functions for calling 'compute' for several compute classes
+    // -->
+    template <std::size_t ID = 0, class data_t>
+    ALWAYS_INLINE
+        typename std::enable_if<ID == sizeof...(compute_ts), void>::type
+        call_compute_helper(Cell<data_t> current_cell) const
+    {
+    } // If we have reached the end of the tuple do nothing
+
+    template <std::size_t ID = 0, class data_t>
+        ALWAYS_INLINE typename std::enable_if <
+        ID<sizeof...(compute_ts), void>::type
+        call_compute_helper(Cell<data_t> current_cell) const
+    {
+        std::get<ID>(m_compute_tuple)
+            .compute(current_cell); // Call compute for the current component
+        call_compute_helper<ID + 1>(
+            current_cell); // call again for next component
+    }
+    // End: Helper functions for calling 'compute' for several compute classes
+
+  public:
+    ComputePack(const std::tuple<compute_ts...> &compute_tuple)
+        : m_compute_tuple(compute_tuple)
+    {
+    }
+
+    template <class data_t>
+    void call_compute(const Cell<data_t> &current_cell) const
+    {
+        call_compute_helper(current_cell);
+    }
+};
+
+/// Helper struct that checks whether a given argument is a compute pack
+template <typename... Ts> struct is_compute_pack : public std::false_type
+{
+};
+
+template <typename... Ts>
+struct is_compute_pack<ComputePack<Ts...>> : public std::true_type
+{
+};
+// End: Helper struct for checking whether a given template argument is a
+// compute pack
+
+#endif /* COMPUTEPACK_HPP_ */
