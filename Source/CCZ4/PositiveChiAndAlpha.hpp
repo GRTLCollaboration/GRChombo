696ac099d78f28e061a4f123dff14bbe9ba38bdb
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2018-01-29 12:14:09 +0000

    Added CCZ4 algebraic constrains.
    Added compute classes that enforce CCZ4 algebraic constraints, incl:
    * Enforcing a trace free A
    * Enforcing positive chi and lapse

---
+++ Source/CCZ4/PositiveChiAndAlpha.hpp
@@ -0,0 +1,30 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+// This compute class enforces the positive chi and alpha condition
+#ifndef POSITIVECHIANDALPHA_HPP_
+#define POSITIVECHIANDALPHA_HPP_
+
+#include "Cell.hpp"
+#include "UserVariables.hpp"
+#include "simd.hpp"
+
+class PositiveChiAndAlpha
+{
+  public:
+    template <class data_t> void compute(Cell<data_t> current_cell) const
+    {
+        auto chi = current_cell.load_vars(c_chi);
+        auto lapse = current_cell.load_vars(c_lapse);
+
+        chi = simd_max(chi, 1e-4);
+        lapse = simd_max(lapse, 1e-4);
+
+        current_cell.store_vars(chi, c_chi);
+        current_cell.store_vars(lapse, c_lapse);
+    }
+};
+
+#endif /* POSITIVECHIANDALPHA_HPP_ */
