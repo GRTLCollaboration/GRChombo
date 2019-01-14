0b1a8d3f4c9069d2bac8462fcc1a0860e4ea54c0
Miren Radia <miren_radia@yahoo.co.uk>
2019-01-14 17:15:43 +0000

    Removed MPI IO functions and C++-ified.

--- Source/utils/WeylExtraction.impl.hpp
+++ Source/utils/WeylExtraction.impl.hpp
@@ -75,8 +75,9 @@ inline void WeylExtraction::execute_query(
     write_integral(integral22, "Weyl_integral_22");
 
     // This generates a lot of output, and it is usually better to output plot
-    // files so for now it is commented out write_extraction("ExtractionOut_",
-    // m_state_ptr_re, m_state_ptr_im);
+    // files so for now it is commented out
+    // write_extraction("ExtractionOut_", m_state_ptr_re.get(),
+    //                  m_state_ptr_im.get());
 }
 
 //! integrate over a spherical shell with given harmonics
@@ -134,107 +135,84 @@ WeylExtraction::integrate_surface(int es, int el, int em,
 
 //! Write out calculated value of integral
 inline void WeylExtraction::write_integral(std::array<double, 2> integral,
-                                           const char *filename) const
+                                           std::string filename) const
 {
     int rank;
 #ifdef CH_MPI
     MPI_Comm_rank(Chombo_MPI::comm, &rank);
-    MPI_File mpi_file;
-    MPI_File_open(Chombo_MPI::comm, filename,
-                  MPI_MODE_CREATE | MPI_MODE_WRONLY | MPI_MODE_APPEND,
-                  MPI_INFO_NULL, &mpi_file);
 #else
     rank = 0;
-    FILE *file;
-    file = fopen(filename, "a");
 #endif
     // only rank 0 does the write out
     if (rank == 0)
     {
-        constexpr int char_length = 60;
+        std::ofstream outfile;
+        // overwrite file if this is the first timestep, otherwise append.
+        if (m_time == m_dt)
+        {
+            outfile.open(filename);
+        }
+        else
+        {
+            outfile.open(filename, std::ios_base::app);
+        }
+        if (!outfile)
+        {
+            MayDay::Error(
+                "WeylExtraction::write_integral: error opening output file");
+        }
+
         // Header data at first timestep
         if (m_time == m_dt)
         {
-            char header[char_length];
-            snprintf(header, char_length,
-                     "# m_time      integral Re   integral Im \n");
-#ifdef CH_MPI
-            MPI_File_write(mpi_file, header, strlen(header), MPI_CHAR,
-                           MPI_STATUS_IGNORE);
-#else
-            fwrite(header, sizeof(char), strlen(header), file);
-#endif
+            outfile << "#" << std::setw(9) << "time";
+            outfile << std::setw(20) << "integral Re";
+            outfile << std::setw(20) << "integral Im" << std::endl;
         }
+
         // Now the data
-        char data[char_length];
-        snprintf(data, char_length, "%f     %e     %e \n", m_time, integral[0],
-                 integral[1]);
-#ifdef CH_MPI
-        MPI_File_write(mpi_file, data, strlen(data), MPI_CHAR,
-                       MPI_STATUS_IGNORE);
-#else
-        fwrite(data, sizeof(char), strlen(data), file);
-#endif
+        outfile << std::fixed << std::setw(10) << m_time;
+        outfile << std::scientific << std::setw(20) << std::setprecision(10);
+        outfile << integral[0] << std::setw(20) << integral[1] << std::endl;
+        outfile.close();
     }
-#ifdef CH_MPI
-    MPI_File_close(&mpi_file);
-#else
-    fclose(file);
-#endif
 }
 
 //! Write out the result of the extraction in phi and theta at each timestep
-inline void WeylExtraction::write_extraction(char *file_prefix,
+inline void WeylExtraction::write_extraction(std::string file_prefix,
                                              const double *m_state_ptr_re,
                                              const double *m_state_ptr_im) const
 {
     int rank;
 #ifdef CH_MPI
     MPI_Comm_rank(Chombo_MPI::comm, &rank);
-    MPI_File mpi_file;
 #else
     rank = 0;
-    FILE *file;
-#endif
-    char step_str[10];
-    sprintf(step_str, "%d", (int)round(m_time / m_dt));
-    char filename[20] = "";
-    strcat(filename, file_prefix);
-    strcat(filename, step_str);
-#ifdef CH_MPI
-    MPI_File_open(Chombo_MPI::comm, filename,
-                  MPI_MODE_CREATE | MPI_MODE_WRONLY | MPI_MODE_APPEND,
-                  MPI_INFO_NULL, &mpi_file);
-#else
-    file = fopen(filename, "a");
 #endif
     // only rank 0 does the write out
     if (rank == 0)
     {
-        // Header data
-        char comp_str_re[20];
-        char comp_str_im[20];
-        sprintf(comp_str_re, UserVariables::variable_names[m_re_comp]);
-        sprintf(comp_str_im, UserVariables::variable_names[m_im_comp]);
-        char header1[20];
-        snprintf(header1, 20, "# m_time : %f \n", m_time);
-#ifdef CH_MPI
-        MPI_File_write(mpi_file, header1, strlen(header1), MPI_CHAR,
-                       MPI_STATUS_IGNORE);
-#else
-        fwrite(header1, sizeof(char), strlen(header1), file);
-#endif
-        char header2[70] = "";
-        strcat(header2, "# theta     phi     ");
-        strcat(header2, comp_str_re);
-        strcat(header2, comp_str_im);
-        strcat(header2, "/n");
-#ifdef CH_MPI
-        MPI_File_write(mpi_file, header2, strlen(header2), MPI_CHAR,
-                       MPI_STATUS_IGNORE);
-#else
-        fwrite(header2, sizeof(char), strlen(header2), file);
-#endif
+        // set up file names and component names
+        int step = std::round(m_time / m_dt);
+        std::string file_str = file_prefix + std::to_string(step);
+        std::string comp_str_re = UserVariables::variable_names[m_re_comp];
+        std::string comp_str_im = UserVariables::variable_names[m_im_comp];
+
+        // write out extraction data to a separate file at each step
+        std::ofstream outfile;
+        outfile.open(file_str);
+        if (!outfile)
+        {
+            MayDay::Error(
+                "WeylExtraction::write_extraction: error opening output file");
+        }
+
+        // header data
+        outfile << "# time : " << m_time << "\n";
+        outfile << "#" << std::setw(11) << "theta";
+        outfile << std::setw(12) << "phi";
+        outfile << std::setw(20) << comp_str_re;
+        outfile << std::setw(20) << comp_str_im << "\n";
 
         // Now the data
         for (int idx = 0; idx < m_num_points; ++idx)
@@ -244,22 +222,15 @@ inline void WeylExtraction::write_extraction(char *file_prefix,
             // don't put a point at z = 0
             double theta = (itheta + 0.5) * m_dtheta;
             double phi = iphi * m_dphi;
-            char data[70];
-            snprintf(data, 70, "%f     %f     %e     %e \n", theta, phi,
-                     m_state_ptr_re[idx], m_state_ptr_im[idx]);
-#ifdef CH_MPI
-            MPI_File_write(mpi_file, data, strlen(data), MPI_CHAR,
-                           MPI_STATUS_IGNORE);
-#else
-            fwrite(data, sizeof(char), strlen(data), file);
-#endif
+            outfile << std::fixed << std::setprecision(7);
+            outfile << std::setw(12) << theta;
+            outfile << std::setw(12) << phi;
+            outfile << std::scientific << std::setprecision(10);
+            outfile << std::setw(20) << m_state_ptr_re[idx];
+            outfile << std::setw(20) << m_state_ptr_im[idx] << "\n";
         }
+        outfile.close();
     }
-#ifdef CH_MPI
-    MPI_File_close(&mpi_file);
-#else
-    fclose(file);
-#endif
 }
 
 #endif /* WEYLEXTRACTION_IMPL_HPP_ */
