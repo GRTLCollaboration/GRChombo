6b1d6403da0a0209488e649ee5de587702cd6572
Miren Radia <miren_radia@yahoo.co.uk>
2019-03-28 14:53:59 +0000

    Separated BoundaryConditions definitions and declarations. Fixed boundary ghost exchange bug.

--- Source/GRChomboCore/GRAMRLevel.cpp
+++ Source/GRChomboCore/GRAMRLevel.cpp
@@ -281,7 +281,7 @@ void GRAMRLevel::regrid(const Vector<Box> &a_new_grids)
     m_state_new.define(level_domain, NUM_VARS, iv_ghosts);
 
     // maintain interlevel stuff
-    m_exchange_copier.exchangeDefine(level_domain, iv_ghosts);
+    defineExchangeCopier(level_domain);
     m_coarse_average.define(level_domain, NUM_VARS, m_ref_ratio);
     m_fine_interp.define(level_domain, NUM_VARS, m_ref_ratio, m_problem_domain);
 
@@ -336,7 +336,7 @@ void GRAMRLevel::initialGrid(const Vector<Box> &a_new_grids)
     m_state_new.define(level_domain, NUM_VARS, iv_ghosts);
     m_state_old.define(level_domain, NUM_VARS, iv_ghosts);
 
-    m_exchange_copier.exchangeDefine(level_domain, iv_ghosts);
+    defineExchangeCopier(level_domain);
     m_coarse_average.define(level_domain, NUM_VARS, m_ref_ratio);
     m_fine_interp.define(level_domain, NUM_VARS, m_ref_ratio, m_problem_domain);
 
@@ -633,7 +633,8 @@ void GRAMRLevel::readCheckpointLevel(HDF5Handle &a_handle)
 
     // maintain interlevel stuff
     IntVect iv_ghosts = m_num_ghosts * IntVect::Unit;
-    m_exchange_copier.exchangeDefine(level_domain, iv_ghosts);
+
+    defineExchangeCopier(level_domain);
     m_coarse_average.define(level_domain, NUM_VARS, m_ref_ratio);
     m_fine_interp.define(level_domain, NUM_VARS, m_ref_ratio, m_problem_domain);
 
@@ -901,4 +902,20 @@ void GRAMRLevel::copyBdyGhosts(const GRLevelData &a_src, GRLevelData &a_dest)
         m_boundaries.copy_boundary_cells(Side::Hi, a_src, a_dest);
         m_boundaries.copy_boundary_cells(Side::Lo, a_src, a_dest);
     }
+}
+
+void GRAMRLevel::defineExchangeCopier(const DisjointBoxLayout &a_level_grids)
+{
+    // if there are Sommerfeld BCs, expand boxes along those sides
+    if (m_p.nonperiodic_boundaries_exist)
+    {
+        m_boundaries.expand_grids_to_boundaries(m_grown_grids, a_level_grids);
+    }
+    else
+    { // nothing to do if periodic BCs
+        m_grown_grids = a_level_grids;
+    }
+
+    IntVect iv_ghosts = m_num_ghosts * IntVect::Unit;
+    m_exchange_copier.exchangeDefine(m_grown_grids, iv_ghosts);
 }
--- Source/GRChomboCore/GRAMRLevel.hpp
+++ Source/GRChomboCore/GRAMRLevel.hpp
@@ -168,6 +168,10 @@ class GRAMRLevel : public AMRLevel, public InterpSource
     /// (for non-periodic boundary conditions, where boundaries evolve via rhs)
     virtual void copyBdyGhosts(const GRLevelData &a_src, GRLevelData &a_dest);
 
+    /// This function is used to define the exchange copiers required for
+    /// copying ghost cells between boxes
+    virtual void defineExchangeCopier(const DisjointBoxLayout &a_level_domain);
+
     BoundaryConditions m_boundaries; // the class for implementing BCs
 
     GRLevelData m_state_old; //!< the solution at the old time
@@ -190,7 +194,9 @@ class GRAMRLevel : public AMRLevel, public InterpSource
     FourthOrderFineInterp m_fine_interp; //!< executes the interpolation from
                                          //!< coarse to fine when regridding
 
-    DisjointBoxLayout m_grids; //!< Holds grid setup (the layout of boxes)
+    DisjointBoxLayout m_grids;       //!< Holds grid setup (the layout of boxes)
+    DisjointBoxLayout m_grown_grids; //!< Holds grown grid setup (for
+                                     //!< Sommerfeld BCs)
 
   public:
     const int m_num_ghosts; //!< Number of ghost cells
