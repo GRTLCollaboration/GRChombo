2682c8fcb9694f11530567da500ba6a77917c9bf
cjoana <c.joana@uclouvain.be>
2019-04-11 15:35:40 +0200

    Created Example/DustField

---
+++ Examples/DustField/Main_ScalarField.cpp
@@ -0,0 +1,55 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#include "parstream.H" //Gives us pout()
+#include <iostream>
+
+#include "DefaultLevelFactory.hpp"
+#include "GRAMR.hpp"
+#include "GRParmParse.hpp"
+#include "SetupFunctions.hpp"
+#include "SimulationParameters.hpp"
+
+// Problem specific includes:
+#include "ScalarFieldLevel.hpp"
+
+int runGRChombo(int argc, char *argv[])
+{
+    // Load the parameter file and construct the SimulationParameter class
+    // To add more parameters edit the SimulationParameters file.
+    char *in_file = argv[1];
+    GRParmParse pp(argc - 2, argv + 2, NULL, in_file);
+    SimulationParameters sim_params(pp);
+
+    // The line below selects the problem that is simulated
+    // (To simulate a different problem, define a new child of AMRLevel
+    // and an associated LevelFactory)
+    GRAMR gr_amr;
+    DefaultLevelFactory<ScalarFieldLevel> scalar_field_level_fact(gr_amr,
+                                                                  sim_params);     // CJ See DefaultLevelFactory.hpp
+    setupAMRObject(gr_amr, scalar_field_level_fact);                               // CJ See SetupFunctions.hpp
+
+
+    // Engage! Run the evolution
+    gr_amr.run(sim_params.stop_time, sim_params.max_steps);
+    gr_amr.conclude();
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    mainSetup(argc, argv);
+
+    int status = runGRChombo(argc, argv);
+
+    if (status == 0)
+        pout() << "GRChombo finished." << std::endl;
+    else
+        pout() << "GRChombo failed with return code " << status << std::endl;
+
+    mainFinalize();
+    return status;
+}
