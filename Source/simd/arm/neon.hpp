ceaf38860cee08a20c134133dda8fe472dcb8add
Kacper Kornet <kk562@damtp.cam.ac.uk>
2019-09-11 11:26:56 +0100

    Rearrange order of functions in neon.hpp

--- Source/simd/arm/neon.hpp
+++ Source/simd/arm/neon.hpp
@@ -13,8 +13,7 @@ template <> struct simd_traits<double>
 template <> struct simd<double> : public simd_base<double>
 {
     typedef typename simd_traits<double>::data_t data_t;
-    typedef uint64x2_t mask_t;
-    static const int simd_len = 2;
+    typedef typename simd_traits<double>::mask_t mask_t;
 
     ALWAYS_INLINE
     simd() : simd_base<double>(vdupq_n_f64(0.)){ }
@@ -29,53 +28,34 @@ template <> struct simd<double> : public simd_base<double>
     static simd load(const double *ptr) { return vld1q_f64(ptr); }
 
     ALWAYS_INLINE
-    simd &operator+=(const simd &a)
-    {
-        m_value = vaddq_f64(m_value, a.m_value);
-        return *this;
-    }
-
-    ALWAYS_INLINE
     static void store(double *ptr, const simd &a)
     {
          vst1q_f64(ptr, a.m_value);
     }
 
-    friend ALWAYS_INLINE mask_t simd_compare_gt(const simd &a, const simd &b)
+    ALWAYS_INLINE
+    simd &operator+=(const simd &a)
     {
-        return  vcgtq_f64(a, b);
+        m_value = vaddq_f64(m_value, a.m_value);
+        return *this;
     }
-
-    friend ALWAYS_INLINE mask_t simd_compare_lt(const simd &a, const simd &b)
-    {
-        return vcltq_f64(a, b);
-
-    }
-
     ALWAYS_INLINE
     simd &operator-=(const simd &a)
     {
         m_value = vsubq_f64(m_value, a.m_value);
         return *this;
     }
-
     ALWAYS_INLINE
     simd &operator*=(const simd &a)
     {
         m_value = vmulq_f64(m_value, a.m_value);
         return *this;
     }
-
     ALWAYS_INLINE
     simd &operator/=(const simd &a)
     {
         m_value = vdivq_f64(m_value, a.m_value);
         return *this;
-    }
-
-    friend ALWAYS_INLINE simd simd_max(const simd &a, const simd &b)
-    {
-        return vmaxq_f64(a, b);
     }
 
     friend ALWAYS_INLINE simd simd_conditional(const mask_t cond,
@@ -84,6 +64,27 @@ template <> struct simd<double> : public simd_base<double>
     {
         return vbslq_f64(cond, false_value, true_value);
     }
+
+    friend ALWAYS_INLINE mask_t simd_compare_lt(const simd &a, const simd &b)
+    {
+        return vcltq_f64(a, b);
+    }
+
+    friend ALWAYS_INLINE mask_t simd_compare_gt(const simd &a, const simd &b)
+    {
+        return  vcgtq_f64(a, b);
+    }
+
+    friend ALWAYS_INLINE simd simd_min(const simd &a, const simd &b)
+    {
+        return vminq_f64(a, b);
+    }
+
+    friend ALWAYS_INLINE simd simd_max(const simd &a, const simd &b)
+    {
+        return vmaxq_f64(a, b);
+    }
+
 };
 
 
