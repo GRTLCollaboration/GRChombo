474c9fb3092558d96bcb395de2e567132eefa959
zainabnazari <42781686+zainabnazari@users.noreply.github.com>
2020-03-31 12:18:49 +0200

    Add marconi Make.defs.local and module script

---
+++ InstallNotes/MakeDefsLocalExamples/cineca_marconi.modules.sh
@@ -0,0 +1,8 @@
+module purge
+module load autoload
+module load intel/pe-xe-2018--binary
+module load intelmpi/2018--binary
+module load zlib/1.2.8--gnu--6.1.0
+module load hdf5/1.8.18--intelmpi--2018--binary
+module load gnu/7.3.0
+module load mkl/2018--binary
