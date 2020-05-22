2862a43723e8659346197d57fa92a28387f451db
Miren Radia <miren_radia@yahoo.co.uk>
2020-05-22 10:24:04 +0100

    Add at_level_timestep_multiple function
    This is a member function of GRAMRLevel which can check whether the
    current time on the level is equal to a multiple of the timestep on the
    target level.

--- Source/GRChomboCore/GRAMRLevel.hpp
+++ Source/GRChomboCore/GRAMRLevel.hpp
@@ -156,6 +156,12 @@ class GRAMRLevel : public AMRLevel, public InterpSource
 
     double get_dx() const;
 
+    /// Returns true if m_time is the same as the time at the end of the current
+    /// timestep on level a_level and false otherwise
+    /// Useful to check whether to calculate something in postTimeStep (which
+    /// might only be needed at the end of a_level's timestep)
+    bool at_level_timestep_multiple(int a_level) const;
+
     /// Fill all ghosts cells
     virtual void fillAllGhosts();
 
