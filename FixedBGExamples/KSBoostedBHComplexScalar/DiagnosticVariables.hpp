63ce2ecf071748f2a2a6bcf5a63bead37dd63a48
KAClough <katyclough@yahoo.com>
2021-03-04 16:44:52 +0000

    Add files for KCBoost

---
+++ FixedBGExamples/KSBoostedBHComplexScalar/DiagnosticVariables.hpp
@@ -0,0 +1,31 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef DIAGNOSTICVARIABLES_HPP
+#define DIAGNOSTICVARIABLES_HPP
+
+// assign an enum to each variable
+enum
+{
+    c_chi,    // the conformal factor of the metric, not evolved
+    c_rho,    // the energy density of the SF
+    c_rhoJ,   // the energy density of the SF
+    c_xMom,   // the energy density of the SF
+    c_Source, // the energy density of the SF
+    c_Edot,   // the energy density of the SF
+    c_Jdot,   // the energy density of the SF
+    c_Stress, // the energy density of the SF
+    c_BHMom,  // the energy density of the SF
+
+    NUM_DIAGNOSTIC_VARS
+};
+
+namespace DiagnosticVariables
+{
+static const std::array<std::string, NUM_DIAGNOSTIC_VARS> variable_names = {
+    "chi", "rho", "rhoJ", "xMom", "Source", "Edot", "Jdot", "Stress", "BHMom"};
+}
+
+#endif /* DIAGNOSTICVARIABLES_HPP */
