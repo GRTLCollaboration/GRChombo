ec68db4478ff731e5d0f951c5b93398c2c5e7f06
Miren Radia <miren_radia@yahoo.co.uk>
2021-06-08 18:28:28 +0100

    Update fawcett Make.defs.local and params file
    BinaryBH params_fawcett.txt if for GRChombo training on fawcett. It
    should fit comfortably on 40 cosmosx cores.
    
    The Make.defs.local is a bit cleaner and the the hdf5 module is changed.

--- InstallNotes/MakeDefsLocalExamples/fawcett-modules.sh
+++ InstallNotes/MakeDefsLocalExamples/fawcett-modules.sh
@@ -2,4 +2,4 @@
 # To load the modules below call this file as an argument to `source`
 module load intel/compilers/2018.3
 module load intel/impi/2018.3/intel
-module load hdf5/1.10.1/intel-18.0.3-wiegkjg
+module load hdf5-intel/1.10.4
