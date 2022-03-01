1c0ac9b520257dfaf1d03fedf2cc675f4e5db709
TaigoFr <taigo.fr@outlook.com>
2022-03-01 22:23:36 +0000

    Fix warning for 2D

--- Source/utils/TensorAlgebra.hpp
+++ Source/utils/TensorAlgebra.hpp
@@ -291,9 +291,9 @@ ALWAYS_INLINE Tensor<2, data_t> lower_all(const Tensor<2, data_t> &tensor_UU,
 constexpr int delta(int i, int j) { return (i == j); }
 
 /// Computes the levi-civita symbol (3D, NB, symbol, not the Tensor)
-inline Tensor<3, double> epsilon()
+inline Tensor<3, double, 3> epsilon()
 {
-    Tensor<3, double> epsilon = {0.};
+    Tensor<3, double, 3> epsilon = {0.};
     epsilon[0][1][2] = 1.0;
     epsilon[1][2][0] = 1.0;
     epsilon[2][0][1] = 1.0;
