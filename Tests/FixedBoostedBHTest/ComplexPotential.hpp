5f6e85fa05391bd8665262ed9778a7aacaa99c85
Dina Traykova <dtraykova@sakura01.bc.rzg.mpg.de>
2022-04-29 11:34:18 +0200

    Add FixedBG BoostedBH ComplexSF test

---
+++ Tests/FixedBoostedBHTest/ComplexPotential.hpp
@@ -0,0 +1,37 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef COMPLEXPOTENTIAL_HPP_
+#define COMPLEXPOTENTIAL_HPP_
+
+#include "simd.hpp"
+
+class ComplexPotential
+{
+  protected:
+    const double m_mu;
+
+  public:
+    //! The constructor
+    ComplexPotential(const double a_mu) : m_mu(a_mu) {}
+
+    //! Set the potential function for the scalar field here
+    template <class data_t, template <typename> class vars_t>
+    void compute_potential(data_t &V_of_phi, data_t &dVdphi_Re,
+                           data_t &dVdphi_Im, const vars_t<data_t> &vars) const
+    {
+        // The potential value at phi
+        // 1/2 m^2 phi^2
+        V_of_phi = 0.5 * m_mu * m_mu * vars.phi_Re * vars.phi_Re +
+                   0.5 * m_mu * m_mu * vars.phi_Im * vars.phi_Im;
+
+        // The potential gradient at phi wrt the real and im field
+        // m^2 phi
+        dVdphi_Re = m_mu * m_mu * vars.phi_Re;
+        dVdphi_Im = m_mu * m_mu * vars.phi_Im;
+    }
+};
+
+#endif /* COMPLEXPOTENTIAL_HPP_ */
---
+++ Tests/FixedBoostedBHTest/UserVariables.hpp
@@ -0,0 +1,42 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef USERVARIABLES_HPP
+#define USERVARIABLES_HPP
+
+#include "ArrayTools.hpp"
+#include "CCZ4UserVariables.hpp"
+#include "EmptyDiagnosticVariables.hpp"
+
+// assign an enum to each variable
+enum
+{
+    // Note that it is important that the first enum value is set to 1 more than
+    // the last CCZ4 var enum
+    c_phi_Re = NUM_CCZ4_VARS, // matter field added
+    c_phi_Im,
+    c_Pi_Re,
+    c_Pi_Im,
+    c_Ham,
+    c_Mom1, //(minus) conjugate momentum
+    c_Mom2, //(minus) conjugate momentum
+    c_Mom3, //(minus) conjugate momentum
+
+    NUM_VARS
+};
+
+namespace UserVariables
+{
+static const std::array<std::string, NUM_VARS - NUM_CCZ4_VARS>
+    user_variable_names = {"phi_Re", "phi_Im", "Pi_Re", "Pi_Im",
+                           "Ham",    "Mom1",   "Mom2",  "Mom3"};
+
+static const std::array<std::string, NUM_VARS> variable_names =
+    ArrayTools::concatenate(ccz4_variable_names, user_variable_names);
+} // namespace UserVariables
+
+//#include "UserVariables.inc.hpp"
+
+#endif /* USERVARIABLES_HPP */
