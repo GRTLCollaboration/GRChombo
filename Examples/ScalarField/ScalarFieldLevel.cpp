a359bd6ce0e40f1f5227d93e8ba2a61d4f8c79fa
Ericka Ann Florio <eaf49@mn01.maths.cam.ac.uk>
2023-07-04 11:41:20 +0100

    means file didn't print...

--- Examples/ScalarField/ScalarFieldLevel.cpp
+++ Examples/ScalarField/ScalarFieldLevel.cpp
@@ -76,7 +76,7 @@ void ScalarFieldLevel::prePlotLevel()
     ScalarFieldWithPotential scalar_field(potential);
     BoxLoops::loop(
         MatterConstraints<ScalarFieldWithPotential>(
-            scalar_field, m_dx, m_p.G_Newton, c_Ham, Interval(c_Mom, c_Mom), PhiBar),
+            scalar_field, m_dx, m_p.G_Newton, c_Ham, Interval(c_Mom, c_Mom)),
         m_state_new, m_state_diagnostics, EXCLUDE_GHOST_CELLS);
 }
 #endif
