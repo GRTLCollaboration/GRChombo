146fc5ef68243a8634bd91cf84ffdc6f842289d2
Ericka Florio <eaf49@cam.ac.uk>
2023-07-21 16:14:20 +0100

    Redid the InitialScalarData file with improvements

--- Examples/ScalarField/myjob.sh
+++ Examples/ScalarField/myjob.sh
@@ -3,12 +3,12 @@
 #! System to be used
 #SBATCH -p knl
 #! Number of nodes to be used
-#SBATCH -N 4
+#SBATCH -N 2
 #! Number of MPI tasks allocated to each node
 #SBATCH --ntasks-per-node=8
 #! Wall clock time required
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #! Name of job
-#SBATCH -J prod
+#SBATCH -J redo-ISD
 
 srun ./Main_ScalarField3d_ch.Linux.64.mpiicpc.ifort.OPTHIGH.MPI.OPENMPCC.ICPX2022.0.ex params.txt
