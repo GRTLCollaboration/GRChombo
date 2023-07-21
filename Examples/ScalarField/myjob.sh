5e53cfa6f51c37dcfc8956d6a1630205d5ab0302
Ericka Florio <eaf49@cam.ac.uk>
2023-07-21 18:13:27 +0100

    submitted production run with L=0.25

--- Examples/ScalarField/myjob.sh
+++ Examples/ScalarField/myjob.sh
@@ -3,12 +3,12 @@
 #! System to be used
 #SBATCH -p knl
 #! Number of nodes to be used
-#SBATCH -N 2
+#SBATCH -N 4
 #! Number of MPI tasks allocated to each node
 #SBATCH --ntasks-per-node=8
 #! Wall clock time required
-#SBATCH --time=02:00:00
+#SBATCH --time=12:00:00
 #! Name of job
-#SBATCH -J L0.25
+#SBATCH -J L0.25-prod
 
 srun ./Main_ScalarField3d_ch.Linux.64.mpiicpc.ifort.OPTHIGH.MPI.OPENMPCC.ICPX2022.0.ex params.txt
