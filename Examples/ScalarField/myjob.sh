e88d70b67d4058cc99e455572f7dee47563588b5
the-florist <eaf49@cam.ac.uk>
2023-11-08 12:02:38 +0000

    submit script name change

--- Examples/ScalarField/myjob.sh
+++ Examples/ScalarField/myjob.sh
@@ -3,14 +3,14 @@
 #! System to be used
 #SBATCH -p knl
 #! Number of nodes to be used
-#SBATCH -N 2
+#SBATCH -N 4
 #! Number of MPI tasks (<=32*nodes)
 #SBATCH --ntasks=8
 #! Number of MPI tasks allocated to each node
 #SBATCH --cpus-per-task=4
 #! Wall clock time required
-#SBATCH --time=02:00:00
+#SBATCH --time=12:00:00
 #! Name of job
-#SBATCH -J slurm-check-test
+#SBATCH -J cosmic-time-check
 
 srun ./Main_ScalarField3d_ch.Linux.64.mpiicpc.ifort.OPTHIGH.MPI.OPENMPCC.ICPX2022.0.ex params.txt
