84e997580accbc11bd8ab40cb8f98a1401c11db1
KAClough <katyclough@yahoo.com>
2024-12-02 17:35:29 +0000

    Add some more edits

--- Examples/ScalarFieldBH/SimulationParameters.hpp
+++ Examples/ScalarFieldBH/SimulationParameters.hpp
@@ -12,7 +12,6 @@
 
 // Problem specific includes:
 #include "InitialScalarData.hpp"
-#include "KerrBH.hpp"
 #include "Potential.hpp"
 
 class SimulationParameters : public SimulationParametersBase
@@ -36,11 +35,6 @@ class SimulationParameters : public SimulationParametersBase
         pp.load("bh_mass", initial_params.bh_mass, 1.0);
         pp.load("scalar_mass", potential_params.scalar_mass, 0.1);
 
-        // Initial Kerr data
-        pp.load("kerr_mass", kerr_params.mass, 1.0);
-        pp.load("kerr_spin", kerr_params.spin, 0.0);
-        pp.load("kerr_center", kerr_params.center, center);
-
 #ifdef USE_AHFINDER
         double AH_guess =
             8. * initial_params.amplitude * initial_params.amplitude;
@@ -58,28 +52,12 @@ class SimulationParameters : public SimulationParametersBase
         warn_parameter("bh_mass", initial_params.bh_mass,
                        initial_params.bh_mass < 0.1 * L,
                        "gives a BH size greater than 0.1 times the domain L");
-        warn_parameter("kerr_mass", kerr_params.mass, kerr_params.mass >= 0.0,
-                       "should be >= 0.0");
-        check_parameter("kerr_spin", kerr_params.spin,
-                        std::abs(kerr_params.spin) <= kerr_params.mass,
-                        "must satisfy |a| <= M = " +
-                            std::to_string(kerr_params.mass));
-        FOR(idir)
-        {
-            std::string name = "kerr_center[" + std::to_string(idir) + "]";
-            warn_parameter(
-                name, kerr_params.center[idir],
-                (kerr_params.center[idir] >= 0) &&
-                    (kerr_params.center[idir] <= (ivN[idir] + 1) * coarsest_dx),
-                "should be within the computational domain");
-        }
     }
 
     // Initial data for matter and potential and BH
     double G_Newton;
     InitialScalarData::params_t initial_params;
     Potential::params_t potential_params;
-    KerrBH::params_t kerr_params;
 
 #ifdef USE_AHFINDER
     double AH_initial_guess;
---
+++ Examples/ScalarFieldBH/restart_plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+#from pylab import savefig
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+plt.rcParams.update(plt.rcParamsDefault)
+plt.rc('xtick', labelsize=15)
+plt.rc('ytick', labelsize=15)
+plt.rcParams.update({'font.size': 15})
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rcParams.update({'figure.figsize'    :  '6, 4.2'})
+plt.rcParams.update({'figure.autolayout': True})
+
+# Read header
+with open('data/Ham_Lineout.dat', 'r') as file:
+    header = file.readline().strip()
+coord_interp = header.split()
+coord_interp = coord_interp[2:]
+
+# Read data
+ham_data = np.loadtxt('data/Ham_Lineout.dat')
+mom_dat = np.loadtxt('data/Mom_Lineout.dat')
+
+# Make the plot 
+plt.plot(coord_interp, ham_data)
+plt.title('lineout of Ham')
+plt.xlabel('distance from centre', fontsize=14)
+plt.ylabel('Ham', fontsize=14)
+plt.savefig('Ham_Lineout.pdf',dpi=256, bbox_inches='tight',pad_inches = 0.1)
+plt.close()
